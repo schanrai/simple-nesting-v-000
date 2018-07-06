64e8c4efc924e47897ff11f4b3891dd0d4fa6c47
Sushi Chanrai <sushi@pathworks.co>
2018-07-06 22:49:20 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -92,6 +92,9 @@ programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LIS
 programmer_hash		
 end
 
+
+
+
 def changing_alan
 	programmer_hash =
  		{
@@ -111,8 +114,8 @@ def changing_alan
 
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
 		 alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
-
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		     programmer_hash
 end
 
 def adding_to_dennis
