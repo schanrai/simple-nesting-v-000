ea42bd3fee01795f1116ad6340857300e6f49b90
Sushi Chanrai <sushi@pathworks.co>
2018-07-06 22:16:03 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -71,10 +71,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-programmer_hash[:yukihiro_matsumoto] => {
-  	:known_for => "Ruby",
-    :languages => ["LISP", "C"]
- }
 
 	programmer_hash =
  		{
@@ -92,6 +88,10 @@ programmer_hash[:yukihiro_matsumoto] => {
         }
      }
 
+programmer_hash[:yukihiro_matsumoto] => {
+		   	:known_for => "Ruby",
+		     :languages => ["LISP", "C"]
+		  }
 
 end
 
