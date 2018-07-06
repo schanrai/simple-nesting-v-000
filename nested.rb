7781fcef44cf346aade0474bf0d8e83ece8afdb5
Sushi Chanrai <sushi@pathworks.co>
2018-07-06 22:01:29 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -61,7 +61,7 @@ def dennis_ritchies_language
         }
      }
 		#placeholder = ""
- 		programmer_hash[:dennis_ritchie][:languages]
+ 		programmer_hash[:dennis_ritchie][:languages].flatten(1)
 end
 
 
