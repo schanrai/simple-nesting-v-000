724f43f2149f144b6f0457a5149aab1c66a2d534
Sushi Chanrai <sushi@pathworks.co>
2018-07-06 21:11:32 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,9 +15,9 @@ def hopper
           :languages => ["C"]
         }
      }
-programmer_hash [:grace_hopper] 
-
+programmer_hash [:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,7 +37,11 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+		programmer_hash [:grace_hopper][:alan_kay][:known_for]
 end
+
+
 
 def dennis_ritchies_language
 	programmer_hash =
